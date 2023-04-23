daaff430d99c1e2d3ec2f7619b844b705e973821
Prof. 9 <prof9@outlook.com>
2023-04-23 21:31:26 +0200

    Wallpapers test

--- MMBNLC-DLL-Mod/dllmain.cpp
+++ MMBNLC-DLL-Mod/dllmain.cpp
@@ -49,6 +49,11 @@ struct XamlProvider {
 static void(__stdcall* NoesisInit)(void) = NULL;
 static Noesis_Ptr<Noesis_BaseComponent>* (__stdcall* Noesis_GUI_LoadXaml_String)(void *_this, char const *) = NULL;
 static void(__stdcall* Noesis_GUI_SetXamlProvider)(XamlProvider* xamlProvider) = NULL;
+static int(__cdecl* Real__stdio_common_vsprintf_s)(uint64_t Options, char* Buffer, size_t BufferCount, const char* Format, _locale_t Locale, va_list ArgList) = NULL;
+static Noesis_BaseComponent* (__stdcall* Noesis_FrameworkElement_FindName)(void* _this, char const* name) = NULL;
+static void(__stdcall* Noesis_FrameworkElement_BringIntoView)(Noesis_BaseComponent* _this) = NULL;
+
+static void(* Settings_UpdateWallpaperCursor)(void* _this, int32_t newWallpaperIdx, int32_t oldWallpaperIdx) = (void(*)(void*, int32_t, int32_t))0x142FA5790;
 
 void __stdcall MyNoesisInit(void) {
     std::cout << "MyNoesisInit called" << std::endl;
@@ -59,6 +64,20 @@ void __stdcall MyNoesisInit(void) {
 Noesis_Ptr<Noesis_BaseComponent>* MyNoesis_GUI_LoadXaml_String(void *_this, char const* str) {
     std::cout << "Noesis::GUI::LoadXaml_String(" << str << ")" << std::endl;
     return Noesis_GUI_LoadXaml_String(_this, str);
+}
+
+void MySettings_UpdateWallpaperCursor(void* _this, int32_t newWallpaperIdx, int32_t oldWallpaperIdx) {
+    std::cout << "Wallpaper " << oldWallpaperIdx << " -> " << newWallpaperIdx << std::endl;
+    Settings_UpdateWallpaperCursor(_this, newWallpaperIdx, oldWallpaperIdx);
+
+    char strbuf[16] = { 0 };
+    snprintf(strbuf, sizeof(strbuf), "F%02d", newWallpaperIdx);
+    Noesis_BaseComponent *comp = Noesis_FrameworkElement_FindName(_this, strbuf);
+    std::cout << "Comp @ " << comp << std::endl;
+    if (comp != NULL) {
+        Noesis_FrameworkElement_BringIntoView(comp);
+        std::cout << "Scroll into view!" << std::endl;
+    }
 }
 
 void MyNoesis_GUI_SetXamlProvider(XamlProvider *xamlProvider) {
@@ -80,7 +99,7 @@ void MyNoesis_GUI_SetXamlProvider(XamlProvider *xamlProvider) {
             std::stringstream contents;
             contents << xamlfile.rdbuf();
 
-            uint32_t len = contents.tellp();
+            uint32_t len = (uint32_t)contents.tellp();
             uint8_t *newContents = new uint8_t[len]; // should only be called once...
             memcpy(newContents, contents.str().c_str(), len);
 
@@ -92,6 +111,17 @@ void MyNoesis_GUI_SetXamlProvider(XamlProvider *xamlProvider) {
     }
 
     Noesis_GUI_SetXamlProvider(xamlProvider);
+}
+
+int My__stdio_common_vsprintf_s(uint64_t Options, char* Buffer, size_t BufferCount, const char* Format, _locale_t Locale, va_list ArgList) {
+    // Fixup format strings for >= 10
+    if (strcmp(Format, "F0%d") == 0) {
+        Format = "F%02d";
+    }
+    else if (strcmp(Format, "W0%d") == 0) {
+        Format = "W%02d";
+    }
+    return Real__stdio_common_vsprintf_s(Options, Buffer, BufferCount, Format, Locale, ArgList);
 }
 
 extern "C" __declspec(dllexport) BOOL WINAPI ChaudLoaderInit(const uint8_t * userdata, size_t size) {
@@ -119,9 +149,16 @@ extern "C" __declspec(dllexport) BOOL WINAPI ChaudLoaderInit(const uint8_t * use
     std::cout << "DetourUpdateThread() ok" << std::endl;
 
     HMODULE noesisDLL = GetModuleHandle(L"Noesis.dll");
-    std::cout << "Noesis.dll @ " << GetModuleHandle(TEXT("Noesis.dll")) << std::endl;
+    std::cout << "Noesis.dll @ " << noesisDLL << std::endl;
     if (noesisDLL == NULL) {
         std::cout << "Noesis.dll is not loaded!" << std::endl;
+        return false;
+    }
+
+    HMODULE ucrtbase = GetModuleHandle(L"ucrtbase");
+    std::cout << "ucrtbase @ " << ucrtbase << std::endl;
+    if (ucrtbase == NULL) {
+        std::cout << "ucrtbase.dll is not loaded!" << std::endl;
         return false;
     }
 
@@ -155,6 +192,36 @@ extern "C" __declspec(dllexport) BOOL WINAPI ChaudLoaderInit(const uint8_t * use
         Noesis_GUI_LoadXaml_String = (Noesis_Ptr<Noesis_BaseComponent>*(__stdcall*)(void*, char const*))fn;
     }
 
+    {
+        FARPROC fn = GetProcAddress(noesisDLL, "?FindName@FrameworkElement@Noesis@@QEBAPEAVBaseComponent@2@PEBD@Z");
+        if (fn == NULL) {
+            std::cout << "Cannot find Noesis::FrameworkElement::FindName(char *)" << std::endl;
+            return false;
+        }
+        std::cout << "Noesis::FrameworkElement::FindName(char *) @ " << fn << std::endl;
+        Noesis_FrameworkElement_FindName = (Noesis_BaseComponent * (__stdcall*)(void*, char const*))fn;
+    }
+
+    {
+        FARPROC fn = GetProcAddress(noesisDLL, "?BringIntoView@FrameworkElement@Noesis@@QEAAXXZ");
+        if (fn == NULL) {
+            std::cout << "Cannot find Noesis::FrameworkElement::BringIntoView(Noesis::BaseComponent *)" << std::endl;
+            return false;
+        }
+        std::cout << "Noesis::FrameworkElement::BringIntoView(Noesis::BaseComponent *) @ " << fn << std::endl;
+        Noesis_FrameworkElement_BringIntoView = (void (__stdcall*)(Noesis_BaseComponent *))fn;
+    }
+
+    {
+        FARPROC fn = GetProcAddress(ucrtbase, "__stdio_common_vsprintf_s");
+        if (fn == NULL) {
+            std::cout << "Cannot find __stdio_common_vsprintf_s" << std::endl;
+            return false;
+        }
+        std::cout << "__stdio_common_vsprintf_s @ " << fn << std::endl;
+        Real__stdio_common_vsprintf_s = (int(__cdecl *)(uint64_t, char*, size_t, const char*, _locale_t, va_list))fn;
+    }
+
     std::cout << "DetourAttach(" << NoesisInit << ", " << MyNoesisInit << ")" << std::endl;
     if (auto result = DetourAttach((PVOID*)&NoesisInit, (PVOID)MyNoesisInit)) {
         std::cout << "DetourAttach() for Noesis::Init() returned " << result << std::endl;
@@ -166,6 +233,14 @@ extern "C" __declspec(dllexport) BOOL WINAPI ChaudLoaderInit(const uint8_t * use
     }
     if (auto result = DetourAttach((PVOID*)&Noesis_GUI_LoadXaml_String, (PVOID)MyNoesis_GUI_LoadXaml_String)) {
         std::cout << "DetourAttach() for Noesis::GUI::LoadXaml(char *) returned " << result << std::endl;
+        return false;
+    }
+    if (auto result = DetourAttach((PVOID*)&Real__stdio_common_vsprintf_s, (PVOID)My__stdio_common_vsprintf_s)) {
+        std::cout << "DetourAttach() for __stdio_common_vsprintf_s returned " << result << std::endl;
+        return false;
+    }
+    if (auto result = DetourAttach((PVOID*)&Settings_UpdateWallpaperCursor, (PVOID)MySettings_UpdateWallpaperCursor)) {
+        std::cout << "DetourAttach() for Settings_UpdateWallpaperCursor returned " << result << std::endl;
         return false;
     }
     std::cout << "DetourAttach() ok" << std::endl;
